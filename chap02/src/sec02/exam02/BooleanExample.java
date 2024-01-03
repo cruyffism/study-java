6252b181ba5211b12e591bd5f74e89fe2defeef3
minki <mkhong916@gmail.com>
2024-01-03 17:18:55 +0900

    java연습

---
+++ chap02/src/sec02/exam02/BooleanExample.java
@@ -0,0 +1,19 @@
+package sec02.exam02;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		
+		// boolean이 true일때 기본값 false일때 else로 간다!
+		
+		boolean stop = false;
+		if (stop) {
+			System.out.println("중지합니다.");
+
+		} else {
+			System.out.println("시작합니다");
+		}
+
+	}
+
+}
