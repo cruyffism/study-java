dac873b383a81afcb16a34f833809260769138c4
minki <mkhong916@gmail.com>
2024-01-11 20:38:39 +0900

    로또풀기

--- quizzz/src/quiz/Arraylotto.java
+++ quizzz/src/quiz/Arraylotto.java
@@ -9,9 +9,15 @@ public class Arraylotto {
         int[] lotto = new int[45];
         
         for(int i = 0; i < 6; i++){
-            lotto[i] =(int) (Math.random() * 45);
-            System.out.println(lotto[i] + " ");
-        }
+            lotto[i] =(int) (Math.random() * 45)+1;
+            System.out.println("ball[" + i + "] :" + lotto[i]);
+			for(int j=0; j<i; j++) {
+				if(lotto[i] == lotto[j]) {
+					i--;
+					break;
+				}
+			}
     
+        }
     }
 }
