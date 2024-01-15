e5af7b8d20e8c4f0c71ed73553df6ab4df7eb10d
minki <mkhong916@gmail.com>
2024-01-16 08:53:26 +0900

    자바 공부부

---
+++ quizzz/src/quiz/SumAvg.java
@@ -0,0 +1,28 @@
+package quiz;
+
+import java.util.Scanner;
+
+public class SumAvg {
+
+	public static void main(String[] args) {
+		// 크기 5의 배열의 값을 입력(스캐너) 받아서 총점과 평균을 구하는 메서드를 만들어 호출해라 
+		
+			Scanner scan =new Scanner(System.in);
+			System.out.println("정수 5개를 입력하세요.");
+			
+			for(int i=0; i<5; i++) {
+			int in = scan.nextInt();
+			}
+			
+		    System.out.println("입력된 배열의 합은: ");
+	        System.out.println("입력된 배열의 평균은: ");
+	}
+	
+	
+	
+//	public static int sum(int[] a) {
+//		
+//	}
+//	
+
+}
