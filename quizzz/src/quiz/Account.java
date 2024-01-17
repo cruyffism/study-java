5b33940a6da5e7ddf98cbd993e01225aba7aeb07
minki <mkhong916@gmail.com>
2024-01-18 00:49:18 +0900

    올려라

--- quizzz/src/quiz/Account.java
+++ quizzz/src/quiz/Account.java
@@ -3,49 +3,70 @@ package quiz;
 import java.util.Scanner;
 
 public class Account {
+	Scanner scan = new Scanner(System.in);
 
-	static Scanner scan = new Scanner(System.in);
-	
 	String name;
-	int accountNumber; //계좌번호
-	int balance; //잔고
-	
+	int accNumber;
+	int balance;
 
-	Account(String name, int accountNumber, int balance) {
-	 	this.name = name;
-        this.accountNumber = accountNumber;
-        this.balance = balance;
+	Account() { // 기본 생성자
+
 	}
-	
+
+	Account(String name, int accNumber, int balance) { // 인스턴스 생성 시 초기 값 설정하는 생성자
+		this.name = name;
+		this.accNumber = accNumber;
+		this.balance = balance;
+	}
+
+	// 겟&셋 메서드 만들기
+
 	public String getName() {
 		return name;
 	}
-	
-	public int getAccountNum() {
-		return accountNumber;
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public void deposit(int cash) { //입금 메서드
-		System.out.println(cash + "원 입금합니다.");
-		balance += cash;
-		System.out.println("입금완료");
+	public int getAccNumber() {
+		return accNumber;
 	}
-	
-	public void withdraw(int cash) { //출금 메서드
-		System.out.println(cash + "원 출금합니다.");
-		 if(cash > balance) {
-			 System.out.println("잔액 부족");
-		 }else {
-			 this.balance -= cash;
-			 System.out.println("출금 완료");
-		 } 
+
+	public void setAccNumber(int accNumber) {
+		this.accNumber = accNumber;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+
+	public void menu2() { // 상세 메뉴 호출 메서드
+		System.out.println("업무를 선택하세요.");
+		System.out.println("1. 입금");
+		System.out.println("2. 출금");
+		System.out.println("3. 잔액 조회");
+		System.out.println("0. 메인메뉴 돌아가기");
+		System.out.print("> ");
+	}
+
+	public void deposit(int cash) { // 입금 메서드
+		int plusCash = 0;
+		balance += plusCash;
+	}
+
+	public void withdraw(int cash) { // 출금 메서드
+		int minusCash = 0;
+		if (minusCash > balance) {
+			System.out.println("잔액이 부족합니다. 출금이 불가능합니다.");
+		} else {
+			balance -= minusCash;
+		}
 		
 	}
-	
-	public void checkBalance() { //잔액 조회
-		System.out.println("잔액을 조회합니다.");
-		System.out.println("잔액 : " + this.balance);
-	}
-
 }
-
--- quizzz/src/quiz/AccountEx.java
+++ quizzz/src/quiz/AccountEx.java
@@ -3,59 +3,91 @@ package quiz;
 import java.util.Scanner;
 
 public class AccountEx {
-	static Scanner scan = new Scanner(System.in);
-	static Account[] accounts = new Account[100]; //100명분 계좌를 저장할 수 있는 배열 
-	
- 	public static void main(String[] args) {
+
+	public static void main(String[] args) {
+		Account acc = new Account();
+		Scanner scan = new Scanner(System.in);
 		boolean run = true;
+		int cash = 0; // 잔액
 
 		while (run) {
+
 			System.out.println("MIT 뱅크에 오신것을 환영합니다.");
 			System.out.println("메뉴를 선택해 주세요.");
-			System.out.println("1.계좌 생성");
-			System.out.println("2.계좌 업무");
-			System.out.println("3.종료");
+			System.out.println("1. 계좌 생성");
+			System.out.println("2. 계좌 업무");
+			System.out.println("3. 종료");
 			System.out.print("선택> ");
-			
-			int menuNum =  Integer.parseInt(scan.nextLine());
+
+			int menuNum = Integer.parseInt(scan.nextLine());
 
 			switch (menuNum) {
-			case 1: //계좌 생성 기능 
-				System.out.println("계좌를 생성합니다.");
+			case 1:
 				System.out.println("예금주 이름을 입력하세요.");
-				System.out.print(">");
 				String name = scan.nextLine();
+				acc.setName(name);
 				System.out.println("계좌번호를 입력하세요.");
-				System.out.print(">");
-				int accountNumber = Integer.parseInt(scan.nextLine());
+				int accNumber = Integer.parseInt(scan.nextLine());
+				acc.setAccNumber(accNumber);
 				System.out.println("초기 입금액을 입력하세요.");
-				System.out.print(">");
 				int balance = Integer.parseInt(scan.nextLine());
-				
-				Account newAccount = new Account(name, accountNumber,balance);
-				
-				for(int i=0;i<accounts.length;i++) {
-					if(accounts[i] == null) {
-						accounts[i] = newAccount;
+				break;
+
+			case 2:
+				System.out.println("사용하고자 하는 계좌 번호를 입력하세요.");
+				int accNumber2 = Integer.parseInt(scan.nextLine());
+
+				if (accNumber2 == acc.getAccNumber()) {
+					System.out.println(acc.name + "님 환영합니다.");
+					
+					acc.menu2();
+					
+					int subNumber = Integer.parseInt(scan.nextLine());
+					
+					switch (subNumber) {
+					
+					case 0:
+						System.out.println("메인메뉴로 돌아갑니다.");
+						break;
+					
+					case 1:
+						System.out.println("입금 금액을 입력하세요.");
+						cash = Integer.parseInt(scan.nextLine());
+						acc.deposit(cash);
+						System.out.println(cash + "원 입금 완료입니다.");
+						System.out.println(" 입금 완료");
+						break;
+					case 2:	
+						System.out.println("출금 금액을 입력하세요.");
+						cash = Integer.parseInt(scan.nextLine());
+						acc.withdraw(cash);
+						System.out.println(cash + "원 출금 합니다.");
+						System.out.println(" 출금 완료");
+						break;
+					case 3:	
+						System.out.println("잔액을 조회합니다.");
+						cash = Integer.parseInt(scan.nextLine());
+						acc.withdraw(cash);
+						System.out.println(cash + "원 출금 합니다.");
+						System.out.println(" 출금 완료");
 						break;
 					}
+				} else {
+					System.out.println("해당 계좌번호를 찾을 수 없습니다.");
 				}
-				System.out.println("계좌 생성");
+
 				break;
 
-			case 2: //계좌 업무 : 입금, 출금, 조회
-				System.out.println("사용하고자 하는 계좌 번호를 입력하세요.");
-				System.out.print("> ");
-				
-				System.out.println("님 환영합니다.");
-				System.out.println("업무를 선택하세요.");
+			case 3:
+				System.out.println("프로그램 종료합니다.");
+				run = false;
 				break;
-				
-				
-			 case 3:
-
+			default:
+				System.out.println("잘못 입력했습니다.");
+				System.out.println("다시 확인해 주세요.");
 			}
 		}
 		scan.close();
 	}
+
 }
