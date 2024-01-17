19db1cfabec8d972c9f6797e53c45d42b985c2f1
minki <mkhong916@gmail.com>
2024-01-17 17:26:07 +0900

    17일 끝

--- quizzz/src/quiz/Account.java
+++ quizzz/src/quiz/Account.java
@@ -10,62 +10,42 @@ public class Account {
 	int accountNumber; //계좌번호
 	int balance; //잔고
 	
-	Account() {
-		
-	}
-	
+
 	Account(String name, int accountNumber, int balance) {
 	 	this.name = name;
         this.accountNumber = accountNumber;
         this.balance = balance;
 	}
 	
-	public static int menu() { // 메뉴 호출 메서드
-		System.out.println("MIT 뱅크에 오신것을 환영합니다.");
-		System.out.println("메뉴를 선택해 주세요.");
-		System.out.println("1.계좌 생성");
-		System.out.println("2.계좌 업무");
-		System.out.println("3.종료");
-		System.out.print("선택> ");
-		int selectNum = scan.nextInt();
-		return selectNum;
+	public String getName() {
+		return name;
 	}
 	
-	public static int menu2() { // 서브 메뉴 호출 메서드
-		System.out.println("MIT 뱅크에 오신것을 환영합니다.");
-		System.out.println("업무를 선택하세요.");
-		System.out.println("1.입금");
-		System.out.println("2.출금");
-		System.out.println("3.잔액 조회");
-		System.out.println("0.메인 메뉴 돌아가기");
-		System.out.print("> ");
-		int selectNum2 = scan.nextInt();
-		return selectNum2;
+	public int getAccountNum() {
+		return accountNumber;
 	}
-	
 
-	public void deposit(int deposit) { //입금 메서드
-		System.out.println("입금 금액을 입력해주세요.");
-		int input = scan.nextInt();
-		System.out.println(input + "원 입금합니다.");
-		balance += input;
+	public void deposit(int cash) { //입금 메서드
+		System.out.println(cash + "원 입금합니다.");
+		balance += cash;
 		System.out.println("입금완료");
-		System.out.println(name +"님 환영합니다.");
-		System.out.println("업무를 선택하세요.");
+	}
+	
+	public void withdraw(int cash) { //출금 메서드
+		System.out.println(cash + "원 출금합니다.");
+		 if(cash > balance) {
+			 System.out.println("잔액 부족");
+		 }else {
+			 this.balance -= cash;
+			 System.out.println("출금 완료");
+		 } 
 		
 	}
 	
-	public void withdraw(int withdraw) { //출금 메서드
-		System.out.println("출금 금액을 입력해주세요.");
-		int output = scan.nextInt();
-		System.out.println(output + "원 출금합니다.");
-		System.out.println("출금완료");
-		System.out.println(name +"님 환영합니다.");
-		System.out.println("업무를 선택하세요.");
+	public void checkBalance() { //잔액 조회
+		System.out.println("잔액을 조회합니다.");
+		System.out.println("잔액 : " + this.balance);
 	}
-	
 
-	
-	
 }
 
--- quizzz/src/quiz/AccountEx.java
+++ quizzz/src/quiz/AccountEx.java
@@ -3,41 +3,57 @@ package quiz;
 import java.util.Scanner;
 
 public class AccountEx {
-	public static void main(String[] args) {
-		Account acc = new Account();
+	static Scanner scan = new Scanner(System.in);
+	static Account[] accounts = new Account[100]; //100명분 계좌를 저장할 수 있는 배열 
+	
+ 	public static void main(String[] args) {
 		boolean run = true;
-		Scanner scan = new Scanner(System.in);
 
 		while (run) {
+			System.out.println("MIT 뱅크에 오신것을 환영합니다.");
+			System.out.println("메뉴를 선택해 주세요.");
+			System.out.println("1.계좌 생성");
+			System.out.println("2.계좌 업무");
+			System.out.println("3.종료");
+			System.out.print("선택> ");
+			
+			int menuNum =  Integer.parseInt(scan.nextLine());
 
-			acc.menu();
-
-			int selectNum = acc.menu();
-
-			switch (selectNum) {
-
-			case 1:
-
+			switch (menuNum) {
+			case 1: //계좌 생성 기능 
 				System.out.println("계좌를 생성합니다.");
 				System.out.println("예금주 이름을 입력하세요.");
 				System.out.print(">");
-				acc.name = scan.nextLine();
+				String name = scan.nextLine();
 				System.out.println("계좌번호를 입력하세요.");
 				System.out.print(">");
-				acc.accountNumber = Integer.parseInt(scan.nextLine());
+				int accountNumber = Integer.parseInt(scan.nextLine());
 				System.out.println("초기 입금액을 입력하세요.");
 				System.out.print(">");
-				acc.balance = Integer.parseInt(scan.nextLine());
+				int balance = Integer.parseInt(scan.nextLine());
+				
+				Account newAccount = new Account(name, accountNumber,balance);
+				
+				for(int i=0;i<accounts.length;i++) {
+					if(accounts[i] == null) {
+						accounts[i] = newAccount;
+						break;
+					}
+				}
 				System.out.println("계좌 생성");
 				break;
 
-			case 2:
+			case 2: //계좌 업무 : 입금, 출금, 조회
 				System.out.println("사용하고자 하는 계좌 번호를 입력하세요.");
 				System.out.print("> ");
-
+				
 				System.out.println("님 환영합니다.");
 				System.out.println("업무를 선택하세요.");
 				break;
+				
+				
+			 case 3:
+
 			}
 		}
 		scan.close();
